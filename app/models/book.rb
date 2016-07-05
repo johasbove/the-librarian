a121240930ac9bdfce69b515b0b4acf36f690922
Johanna Salazar Bove <johanna.salazar.b@gmail.com>
2016-07-05 12:47:14 -0400

    Portada en index. TODO:Mejorar apariencia!

--- app/models/book.rb
+++ app/models/book.rb
@@ -22,6 +22,6 @@ class Book < ActiveRecord::Base
   validates :title, :author, :editorial, :presence => true
   validates :edition, :publication_year, numericality: { only_integer: true, greater_than: 0 }, allow_nil: true
 
-  has_attached_file :cover, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/default_cover.jpg"
+  has_attached_file :cover, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/default_cover.png"
   validates_attachment_content_type :cover, :content_type => /\Aimage\/.*\Z/
 end
